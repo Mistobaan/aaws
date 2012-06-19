ac1777eecb2a8e3e0b7f3500eab2e3bcee882c38
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2012-06-19 14:46:49 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='aaws',
+      version='0.1',
+      description='Async Amazon Service API',
+      author='',
+      author_email='',
+      url='',
+      packages=['aaws',],
+     )
